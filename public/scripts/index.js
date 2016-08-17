94500c2264f4378705c3ca538e3fd65b3d64a6a3
ruslan <pdbq2121@gmail.com>
2016-08-17 10:50:02 +0300

    fix: -2 var

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,67 +1,62 @@
 /**
  * Created by ruslan on 17.08.16.
  */
-
-var divUpAndDown = React.createClass({
-   render: function(){
-       return (
-           <div className="col-md-12 col-sm-12 col-xs-12 test">
-           </div>
-       );
-   }
-});
-
-var SelectMenu = React.createClass({
-
-   render: function(){
+(function() {
 
 
-       return (
-           <div className="col-md-12 col-sm-12 col-xs-12" id="forPhone">
-               <form action="" className="inputForm">
-                   <input type="text" className="form-control" placeholder="City" />
-               </form>
-           </div>
-       );
-   }
+    var SelectMenu = React.createClass({
 
-});
-
-var SelectMenuApp = React.createClass({
+        render: function () {
 
 
-
-    render: function(){
-var HtmldivUpAndDown = (<divUpAndDown />),
-    HtmlSelectMenu = (<SelectMenu />);
-        return (
-
-            <div className="container">
-                <div className="row">
-                    {HtmldivUpAndDown}
-                        {HtmlSelectMenu}
-                    {HtmldivUpAndDown}
+            return (
+                <div className="col-md-12 col-sm-12 col-xs-12" id="forPhone">
+                    <form action="" className="inputForm">
+                        <input type="text" className="form-control" placeholder="City"/>
+                    </form>
                 </div>
-            </div>
+            );
+        }
+
+    });
+
+    var SelectMenuApp = React.createClass({
+
+
+        render: function () {
+
+             var HtmlSelectMenu = (<SelectMenu />);
+            return (
+
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12 col-sm-12 col-xs-12 test"></div>
+
+                         {HtmlSelectMenu}
+
+                        <div className="col-md-12 col-sm-12 col-xs-12 test"></div>
+                    </div>
+                </div>
                 /*<div className="container">
-                    <div className="row div">
-                        <div className="col-md-12 col-sm-12 col-xs-12" id="test" style="height: 1000px">
-                            <p id="p"></p>
-                        </div>
-                        <div className="col-md-12 col-sm-12 col-xs-12" id="forPhone">
-                            <form action="" className="inputForm">
-                                <input type="text" className="form-control" placeholder="City">
-                            </form>
-                        </div>
-                    </div>
-                    <div className="col-md-12 col-sm-12 col-xs-12" style="height: 1000px">
-                    </div>
-                </div>*/
-        );
-    }
+                 <div className="row div">
+                 <div className="col-md-12 col-sm-12 col-xs-12" id="test" style="height: 1000px">
+                 <p id="p"></p>
+                 </div>
+                 <div className="col-md-12 col-sm-12 col-xs-12" id="forPhone">
+                 <form action="" className="inputForm">
+                 <input type="text" className="form-control" placeholder="City">
+                 </form>
+                 </div>
+                 </div>
+                 <div className="col-md-12 col-sm-12 col-xs-12" style="height: 1000px">
+                 </div>
+                 </div>*/
+            );
+        }
 
 
     });
 
 
-ReactDOM.render(<SelectMenuApp />, document.getElementById('selectMenu'));
+    ReactDOM.render(<SelectMenuApp />, document.getElementById('selectMenu'));
+})();
